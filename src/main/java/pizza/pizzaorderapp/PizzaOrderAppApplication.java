e75617fc5fad3fac7f062f3a9b7f6786af736f5e
petivip <petivip@gmail.com>
2019-12-10 11:02:58 -0500

    initial commit

---
+++ src/main/java/pizza/pizzaorderapp/PizzaOrderAppApplication.java
@@ -0,0 +1,13 @@
+package pizza.pizzaorderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaOrderAppApplication.class, args);
+    }
+
+}
