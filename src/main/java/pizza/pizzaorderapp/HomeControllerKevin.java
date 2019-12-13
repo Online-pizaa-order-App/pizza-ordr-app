e4734bfc78324120f4837b26e34b8e8f0c440f94
kcheung3102 <kcheung3102@gmail.com>
2019-12-13 10:35:38 -0500

    need to fix login styling

--- src/main/java/pizza/pizzaorderapp/HomeControllerKevin.java
+++ src/main/java/pizza/pizzaorderapp/HomeControllerKevin.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
-public class HomeController {
+public class HomeControllerKevin {
     @RequestMapping("/")
     public String landing(Model model) {
         return "landing";
