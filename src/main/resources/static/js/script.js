78e5e6d86de409e82ce2dab34a64cd9f36ede75b
kcheung3102 <kcheung3102@gmail.com>
2019-12-16 14:49:21 -0500

    toggle and button fix

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,3 +4,23 @@ $(document).ready(function() {
 });
 
 new WOW().init();
+
+
+function pepperoniToggle() {
+    document.getElementById('pepperoni-pic').style.visibility =
+        document.getElementById('pepperoni-pic').style.visibility == 'visible'? 'hidden' : 'visible';
+}
+
+function mushroomToggle() {
+    document.getElementById("shroom1").style.visibility =
+        document.getElementById('shroom1').style.visibility == 'visible'? 'hidden' : 'visible';
+
+    document.getElementById("shroom2").style.visibility =
+        document.getElementById('shroom2').style.visibility == 'visible'? 'hidden' : 'visible';
+}
+
+function greenPepperToggle() {
+    document.getElementById("green-pepper").style.visibility =
+        document.getElementById('green-pepper').style.visibility == 'visible'? 'hidden' : 'visible';
+}
+
